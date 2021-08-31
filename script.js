3f4da508338c764a4ac2269e61750dc1593ad45f
cleyderfelipe <84884616+cleyderfelipe@users.noreply.github.com>
2021-08-31 10:25:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,22 @@
- const calcular= document.getElementById('calcular');
-
+ 
+const calcular= document.getElementById('calcular');
    calcular.addEventListener('click',imc);
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
-
- 
 
  function imc(){
 
   const nome= document.getElementById('nome');
-
  const peso= document.getElementById('peso');
-
  const altura= document.getElementById('altura');
-
  const resultado= document.getElementById('resultado');
-
  
-
- 
-
- 
-
    if(nome.value!==''&&peso.value!==''&&altura.value!==''){
 
-     
-
      const a=(peso.value/(altura.value*altura.value)).toFixed(1)
-
-   
-
+ 
    let classi=''
-
-  
-
+   
   if(a<16.9){
-
+    
     classi='muito abaixo do peso, muito cuidado!'
 
     }else if (a<18.4){
@@ -73,23 +44,10 @@
       classi = 'com obesidade grau 3, muito cuidado!'
 
     }
-
-  
-
-  
-
-  
-
-  
-
    resultado.textContent=`${nome.value} seu imc é ${a},e você está ${classi}`;
 
    }else{
-
     resultado.textContent=('preencha todos os campos')
-
    }
-
-   
 
  }
